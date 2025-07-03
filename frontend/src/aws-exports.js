2592ee1f223e74dac4360362ed6528e616e355f8
Jyothi Mani <jyothi@example.com>
2025-07-03 16:47:00 +0100

    changes

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,16 +1,15 @@
-const awsConfig = {
-  Auth: {
-    region: "eu-west-2",
-    userPoolId: "eu-west-2_n6vDQdu5N",
-    userPoolWebClientId: "cncih8g9fhdcnirv8q3vjht7g",
-    oauth: {
-      domain: "eu-west-2n6vdqdu5n.auth.eu-west-2.amazoncognito.com", // ✅ no https
-      scope: ["email", "openid", "phone"],
-      redirectSignIn: "http://localhost:5173/",
-      redirectSignOut: "http://localhost:5173/",
-      responseType: "code"
-    }
+const awsExports = {
+  aws_project_region: "eu-west-2",
+  aws_cognito_region: "eu-west-2",
+  aws_user_pools_id: "eu-west-2_n6vDQdu5N",
+  aws_user_pools_web_client_id: "cncih8g9fhdcnirv8q3vjht7g",
+  oauth: {
+    domain: "eu-west-2n6vdqdu5n.auth.eu-west-2.amazoncognito.com",
+    scope: ["email", "openid", "phone"],
+    redirectSignIn: "http://localhost:5173/",
+    redirectSignOut: "http://localhost:5173/",
+    responseType: "code"
   }
 };
 
-export default awsConfig;
+export default awsExports;
